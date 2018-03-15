b01eac7c56716b2574eb7f46a537f5bb60a7bc15
pleaseduck <pikov.artem@icloud.com>
2018-03-15 15:24:02 +0300

    Поправил шампу и подвал в мобильном режиме

--- app/js/common.js
+++ app/js/common.js
@@ -108,4 +108,18 @@ $("form.callback").submit(function() { //Change
   return false;
 });
 
+$(window).scroll(function () {
+  if ($(this).scrollTop() > $(this).height()) {
+    $('.top').addClass('active');
+  } else {
+    $('.top').removeClass('active');
+  }
+});
+$('.top').click(function () {
+  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
+
+$(window).on('load', function () {
+  $('.preloader').delay(1000).fadeOut('slow');
+})
 });
